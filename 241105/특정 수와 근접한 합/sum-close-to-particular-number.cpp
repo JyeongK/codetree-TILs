61960066d825181cc5a317b105876623e9007e02
JyeongK <76465656+JyeongK@users.noreply.github.com>
2024-11-05 01:42:56 +0900

    Title: 특정 수와 근접한 합, Time: 5ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 241105/특정 수와 근접한 합/sum-close-to-particular-number.cpp
+++ 241105/특정 수와 근접한 합/sum-close-to-particular-number.cpp
@@ -4,22 +4,16 @@ using namespace std;
 int main() {
     // 여기에 코드를 작성해주세요.
     int N, S, sumA=0, thres;
-    char* arr;
+    
     scanf("%d %d", &N, &S);
 
-    arr = new char(N);
+    char arr[N];
 
     for(int i=0; i<N; i++)
     {
         scanf("%d", &arr[i]);
         sumA += arr[i];
     }
-
-    // 전체에 대한 합 - 2개의 합 = 나머지 값 --> S와 유사해야함
-    // 전체에 대한 합 - S -> 2개의 합 유사한 값 탐색
-
-    // 두개의 합의 모든 종류
-
     
     thres = sumA - S;
     int diff = 0;
@@ -46,6 +40,6 @@ int main() {
     }
 
     printf("%d", result);
-    delete arr;
+    // delete[] arr;
     return 0;
 }
